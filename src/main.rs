df1fe29e48cf3522698779e8292372d30f94cf9d
Nils Martel <nils.martel@graphmasters.net>
2019-12-23 12:17:07 +0100

    Enable option to print full path

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
+use std::fs::*;
 fn main() {
+    let arg = if let Some(arg) = std::env::args().nth(1) {
+        &arg == "--path" || &arg == "-p"
+    } else {
+        false
+    };
+
     let path = std::env::var("PATH").unwrap();
 
     let readdir = |dir| std::fs::read_dir(dir).unwrap();
@@ -6,7 +13,13 @@ fn main() {
     path.split(':')
         .filter(|dir| std::path::Path::new(dir).exists())
         .flat_map(readdir)
-        .for_each(|bin| {
-            println!("{}", bin.unwrap().file_name().into_string().unwrap());
-        });
+        .for_each(|entry| print_entry(entry.unwrap(), arg));
+}
+
+fn print_entry(bin: DirEntry, print_path: bool) {
+    if print_path {
+        println!("{}", bin.path().to_str().unwrap());
+    } else {
+        println!("{}", bin.file_name().into_string().unwrap());
+    }
 }
