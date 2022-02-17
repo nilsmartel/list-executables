04555ab389e6419d43816771d92aca1a4c5a8f61
Nils Martel <nilsmartel@yahoo.de>
2022-02-17 11:24:38 +0100

    Merge changes

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # list-executables
 
+<<<<<<< HEAD
 Simple CLI to list all executables in $PATH
+=======
+>>>>>>> Ensure that only executable commands are listed
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 use std::fs::*;
+use std::os::unix::fs::PermissionsExt;
+
 fn main() {
-    let arg = if let Some(arg) = std::env::args().nth(1) {
-        &arg == "--path" || &arg == "-p"
-    } else {
-        false
-    };
+    let arg = std::env::args()
+        .nth(1)
+        .map(|arg| match arg.as_str() {
+            "--path" | "-p" => true,
+            "--help" => print_help(0),
+            _ => print_help(1),
+        })
+        .unwrap_or(false);
 
     let path = std::env::var("PATH").unwrap();
 
@@ -13,13 +18,35 @@ fn main() {
     path.split(':')
         .filter(|dir| std::path::Path::new(dir).exists())
         .flat_map(readdir)
-        .for_each(|entry| print_entry(entry.unwrap(), arg));
+        .map(Result::unwrap)
+        .filter(|entry| {
+            entry
+                .metadata()
+                // check if entry is a filea and if it's executable
+                .map(|meta| meta.is_file() && meta.permissions().mode() & 0o111 != 0)
+                .unwrap_or(false)
+        })
+        .for_each(|entry| print_entry(entry, arg));
 }
 
 fn print_entry(bin: DirEntry, print_path: bool) {
     if print_path {
         println!("{}", bin.path().to_str().unwrap());
-    } else {
-        println!("{}", bin.file_name().into_string().unwrap());
+        return;
     }
+
+    println!("{}", bin.file_name().into_string().unwrap());
+}
+
+fn print_help(code: i32) -> ! {
+    println!(
+        "executables [options]
+        list all executables that are found in the current PATH
+
+        options:
+            -p  --path      print full path to executable
+        "
+    );
+
+    std::process::exit(code);
 }
